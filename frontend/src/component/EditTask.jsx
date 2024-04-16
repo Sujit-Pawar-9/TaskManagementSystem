e1dc186554fd25d5cefaec440431a7b3d9aabec1
Sujit-Pawar-9 <sujitpawar18599@gmail.com>
2024-04-16 12:34:27 +0530

    new

--- frontend/src/component/EditTask.jsx
+++ frontend/src/component/EditTask.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getTaskById, updateTask } from "../service/TaskApiService";
-import '../css/tasks.css'
+import "../css/EditTask.css";
 
 const EditTask = () => {
   const { taskId } = useParams();
@@ -39,40 +39,43 @@ const EditTask = () => {
 
   return (
     <div className="edit-task-container">
-      <h2>Edit Task</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Task title:</label>
+      <h2 className="edit-task-heading">Edit Task</h2>
+      <form className="edit-task-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title" className="form-label">Task title:</label>
           <input
             type="text"
             id="title"
             name="title"
             value={task.title || ""}
             onChange={handleInputChange}
+            className="form-input"
             required
           />
         </div>
-        <div>
-          <label htmlFor="description">Description:</label>
+        <div className="form-group">
+          <label htmlFor="description" className="form-label">Description:</label>
           <textarea
             id="description"
             name="description"
             value={task.description || ""}
             onChange={handleInputChange}
+            className="form-textarea"
             required
           ></textarea>
         </div>
-        <div>
-          <label htmlFor="dueDate">Due Date:</label>
+        <div className="form-group">
+          <label htmlFor="dueDate" className="form-label">Due Date:</label>
           <input
             type="date"
             id="dueDate"
             name="dueDate"
             value={task.dueDate || ""}
             onChange={handleInputChange}
+            className="form-input"
           />
         </div>
-        <button type="submit">Update Task</button>
+        <button type="submit" className="submit-btn">Update Task</button>
       </form>
     </div>
   );
